9c17c1ef8f1908ffe35e46681a12d47a616ae051
Tejas Vedantham <tejas6080@gmail.com>
2021-10-19 16:56:43 -0400

    admin UI for set 1

---
+++ PTTMobile2/app/src/main/java/com/example/tejasvedantham/pttmobile2/CreateUserActivity.java
@@ -0,0 +1,21 @@
+package com.example.tejasvedantham.pttmobile2;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+
+    }
+
+    public void createUser(View view) {
+        // TODO: Create user logic here
+    }
+
+}
--- PTTMobile2/app/src/main/java/com/example/tejasvedantham/pttmobile2/LoginActivity.java
+++ PTTMobile2/app/src/main/java/com/example/tejasvedantham/pttmobile2/LoginActivity.java
@@ -3,12 +3,27 @@ package com.example.tejasvedantham.pttmobile2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public Button loginButton;
+    public EditText usernameField;
+    public EditText passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        usernameField = (EditText) findViewById(R.id.usernameField);
+        passwordField = (EditText) findViewById(R.id.passwordField);
+    }
+
+    public void login(View view) {
+        // TODO: Login logic here
     }
 }
