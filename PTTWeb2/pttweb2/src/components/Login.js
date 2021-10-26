3025e8ad94e72619316cf1eca640f8b114256905
KB Lee <klee869@gatech.edu>
2021-10-25 21:14:04 -0400

    Login feature updated

--- PTTWeb2/pttweb2/src/components/Login.js
+++ PTTWeb2/pttweb2/src/components/Login.js
@@ -1,7 +1,21 @@
 import { Grid, Paper, Avatar, TextField, Button } from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { useHistory } from "react-router-dom";
 
-function Login() {
+function Login({ username, handleChange, users }) {
+  let history = useHistory();
+  const handleSubmit = () => {
+    const userObject = users.find((element) => element.email === username);
+    if (username === "admin") {
+      history.push("/admin");
+    } else {
+      if (userObject) {
+        history.push({ pathname: "/user", state: userObject });
+      } else {
+        alert("User not found");
+      }
+    }
+  };
   const paperStyle = {
     padding: 20,
     height: "60vh",
@@ -18,16 +32,20 @@ function Login() {
           </Avatar>
           <h2>Sign in</h2>
         </Grid>
-        <TextField
-          label="Username"
-          placeholder="Enter username"
-          variant="standard"
-          fullWidth
-          required
-        />
-        <Button type="submit" color="primary" fullWidth variant="contained">
-          Sign in
-        </Button>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="Username"
+            placeholder="Enter username"
+            variant="standard"
+            fullWidth
+            required
+            value={username}
+            onChange={handleChange}
+          />
+          <Button type="submit" color="primary" fullWidth variant="contained">
+            Sign in
+          </Button>
+        </form>
       </Paper>
     </Grid>
   );
