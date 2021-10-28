695712a93d6f713420dddc8d1fd1e55ce7fd3fba
Saira Poonnen <spoonnen3@gatech.edu>
2021-10-27 22:17:47 -0400

    Update login activity

--- PTTMobile2/app/src/main/java/com/example/tejasvedantham/pttmobile2/LoginActivity.java
+++ PTTMobile2/app/src/main/java/com/example/tejasvedantham/pttmobile2/LoginActivity.java
@@ -10,7 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -41,35 +46,80 @@ public class LoginActivity extends AppCompatActivity {
 
     public void login(View view) {
         // TODO: Login logic here
-        String url = "/users";
+//        String url = "/users";
         String email = usernameField.getText().toString();
 
         // “admin" was specified in use cases
         if (email.equals("admin")) {
             startActivity(new Intent(getApplicationContext(), AdminHomeActivity.class));
         }
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url="http://172.16.33.67:8080" +"/users";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,url,null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray jsonArray) {
 
-        backendConnections.ExecuteHTTPRequest(url, Request.Method.GET, null, new BackendConnections.VolleyCallback() {
-            @Override
-            public void onSuccess(JSONObject response) throws JSONException {
-                Log.d(LOG_TAG, String.format("GET %s RES %s", url, response));
-                // TODO: filter the list of all users using the email specified by this user
-                JSONArray users = response.getJSONArray("users");
-                for (int i = 0; i < users.length(); ++i) {
-                    JSONObject user = (JSONObject) users.get(i);
-                    if (user.get("email").equals(email)) {
-                        userSession.setUserId(user.get("id").toString());
+                        try {
+                            JSONArray users = jsonArray;
+                            for (int i = 0; i < users.length(); ++i) {
+                                JSONObject user = (JSONObject) users.get(i);
+                                if (user.get("email").equals(email)) {
+                                    userSession.setUserId(user.get("id").toString());
 
-                        startActivity(new Intent(getApplicationContext(), UserHomeActivity.class));
+                                    startActivity(new Intent(getApplicationContext(), UserHomeActivity.class));
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
-                }
-                Utils.displayExceptionMessage(getApplicationContext(), "Invalid email");
-            }
-
+                },  new Response.ErrorListener() {
             @Override
-            public void onError(VolleyError error) {
-                Log.d(LOG_TAG, String.format("GET %s REQ FAILED", url));
+            public void onErrorResponse(VolleyError error) {
+
             }
         });
+        jsonArrayRequest.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return 5000;
+            }
+
+            @Override
+            public int getCurrentRetryCount() {
+                return 5000;
+            }
+
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
+
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
+
+//        backendConnections.ExecuteHTTPRequest(url, Request.Method.GET, null, new BackendConnections.VolleyCallback() {
+//            @Override
+//            public void onSuccess(JSONObject response) throws JSONException {
+//                Log.d(LOG_TAG, String.format("GET %s RES %s", url, response));
+//                // TODO: filter the list of all users using the email specified by this user
+//                JSONArray users = response.getJSONArray("users");
+//                for (int i = 0; i < users.length(); ++i) {
+//                    JSONObject user = (JSONObject) users.get(i);
+//                    if (user.get("email").equals(email)) {
+//                        userSession.setUserId(user.get("id").toString());
+//
+//                        startActivity(new Intent(getApplicationContext(), UserHomeActivity.class));
+//                    }
+//                }
+//                Utils.displayExceptionMessage(getApplicationContext(), "Invalid email");
+//            }
+//
+//            @Override
+//            public void onError(VolleyError error) {
+////                Log.d(LOG_TAG, String.format("GET %s REQ FAILED", url));
+//                Log.d(LOG_TAG, String.format("GET %s REQ FAILED", error.toString()));
+//            }
+//        });
     }
 }
