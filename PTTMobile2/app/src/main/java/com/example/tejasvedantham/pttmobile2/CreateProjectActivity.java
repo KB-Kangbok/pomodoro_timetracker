c5f70647a075ced2aa6cf1dbbfe04f7d97c281f9
Saira Poonnen <spoonnen3@gatech.edu>
2021-10-28 01:19:22 -0400

    Update create project

--- PTTMobile2/app/src/main/java/com/example/tejasvedantham/pttmobile2/CreateProjectActivity.java
+++ PTTMobile2/app/src/main/java/com/example/tejasvedantham/pttmobile2/CreateProjectActivity.java
@@ -2,6 +2,7 @@ package com.example.tejasvedantham.pttmobile2;
 
 import static com.example.tejasvedantham.pttmobile2.LoginActivity.userSession;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,11 +10,22 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateProjectActivity extends AppCompatActivity {
 
@@ -43,19 +55,38 @@ public class CreateProjectActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        String url = String.format(urlFormat, userId);
 
-        backendConnections.ExecuteHTTPRequest(url, Request.Method.POST, postData, new BackendConnections.VolleyCallback() {
-
+        String url = String.format("http://172.16.33.67:8080" + urlFormat, userId);
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, postData,
+        new Response.Listener< JSONObject >() {
             @Override
-            public void onSuccess(JSONObject response) {
+            public void onResponse(JSONObject response) {
                 Log.d(LOG_TAG, String.format("POST %s RES %s", url, response));
+                startActivity(new Intent(getApplicationContext(), UserHomeActivity.class));
             }
-
+        }, new Response.ErrorListener() {
             @Override
-            public void onError(VolleyError error) {
-                Log.d(LOG_TAG, String.format("POST %s REQ FAILED", url));
+            public void onErrorResponse(VolleyError error) {
+                Log.d("namee", error.toString());
+                error.printStackTrace();
+
             }
         });
+        requestQueue.add(request);
+//    }
+
+//        backendConnections.ExecuteHTTPRequest(url, Request.Method.POST, postData, new BackendConnections.VolleyCallback() {
+//
+//            @Override
+//            public void onSuccess(JSONObject response) {
+//                Log.d(LOG_TAG, String.format("POST %s RES %s", url, response));
+//            }
+
+//            @Override
+//            public void onError(VolleyError error) {
+//                Log.d(LOG_TAG, String.format("POST %s REQ FAILED", url));
+//            }
+//        });
     }
 }
