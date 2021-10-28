35a5d700ea915641d84e495a54cbb56fd67af1c1
Saira Poonnen <spoonnen3@gatech.edu>
2021-10-28 00:19:45 -0400

    Update user home

--- PTTMobile2/app/src/main/java/com/example/tejasvedantham/pttmobile2/UserHomeActivity.java
+++ PTTMobile2/app/src/main/java/com/example/tejasvedantham/pttmobile2/UserHomeActivity.java
@@ -13,7 +13,12 @@ import java.util.ArrayList;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -24,7 +29,7 @@ public class UserHomeActivity extends AppCompatActivity {
     private static final String CONFIRM_MSG = "The project has time already logged to it. Do you still want to delete it?";
 
     private ListView projectListView;
-    private ArrayList<Project> projectList;
+    private ArrayList<Project> projectList = new ArrayList<>();
 
     private Button createProjectPageButton;
 
@@ -52,41 +57,90 @@ public class UserHomeActivity extends AppCompatActivity {
             }
         });
 
-        projectListView = (ListView) findViewById(R.id.admin_user_list);
-
+        projectListView = (ListView) findViewById(R.id.project_list);
+        Log.d("user", userSession.getUserId());
         // Fetch list of user to generate list
-        String url = String.format("/users/%s/projects", userSession.getUserId());
-        backendConnections.ExecuteHTTPRequest(url, Request.Method.GET, null, new BackendConnections.VolleyCallback() {
-            @Override
-            public void onSuccess(JSONObject response) {
-                Log.d(LOG_TAG, String.format("GET $s RES %s", url, response));
+        String url = String.format("http://172.16.33.67:8080/users/%s/projects", userSession.getUserId());
+        Log.d("user", userSession.getUserId());
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+//        String url="http://172.16.33.67:8080" +"/users";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,url,null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray jsonArray) {
 
-                try {
-                    JSONArray jsonArray = new JSONArray(response);
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        JSONObject jsonObject = jsonArray.getJSONObject(i);
-                        String projectname = jsonObject.getString("projectname");
+                        try {
+                            for (int i = 0; i < jsonArray.length(); i++) { ;
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            String projectname = jsonObject.getString("projectname");
 
-                        Project project = new Project(projectname);
-                        projectList.add(project);
+                            Project project = new Project(projectname);
+                            projectList.add(project);
 
+                        }
+
+                        ProjectListAdapter adapter = new ProjectListAdapter(UserHomeActivity.this, projectList);
+                        projectListView.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
-
-                    ProjectListAdapter adapter = new ProjectListAdapter(UserHomeActivity.this, projectList);
-                    projectListView.setAdapter(adapter);
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
+                },  new Response.ErrorListener() {
             @Override
-            public void onError(VolleyError error) {
-                Log.d(LOG_TAG, String.format("GET %s REQ FAILED", url));
+            public void onErrorResponse(VolleyError error) {
+
             }
         });
-    }
+        jsonArrayRequest.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return 5000;
+            }
+
+            @Override
+            public int getCurrentRetryCount() {
+                return 5000;
+            }
+
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
+
+            }
+        });
+        requestQueue.add(jsonArrayRequest);}
+//        backendConnections.ExecuteHTTPRequest(url, Request.Method.GET, null, new BackendConnections.VolleyCallback() {
+//            @Override
+//            public void onSuccess(JSONObject response) {
+//                Log.d(LOG_TAG, String.format("GET $s RES %s", url, response));
+//
+//                try {
+//                    JSONArray jsonArray = new JSONArray(response);
+//                    for (int i = 0; i < jsonArray.length(); i++) {
+//                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+//                        String projectname = jsonObject.getString("projectname");
+//
+//                        Project project = new Project(projectname);
+//                        projectList.add(project);
+//
+//                    }
+//
+//                    ProjectListAdapter adapter = new ProjectListAdapter(UserHomeActivity.this, projectList);
+//                    projectListView.setAdapter(adapter);
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onError(VolleyError error) {
+//                Log.d(LOG_TAG, String.format("GET %s REQ FAILED", error.toString()));
+//            }
+//        });
+//    }
+
 
     public void deleteProject(View view, String projectId) {
         String url = String.format("/users/%s/projects/%s/sessions", userSession.getUserId(), projectId);
