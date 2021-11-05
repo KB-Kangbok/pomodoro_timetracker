92aa139dd64fa92ef08bdb90b3a9677dce5c87e7
jxiang37 <junzhu@gatech.edu>
2021-11-04 21:30:10 -0400

    Modify chromedriver

--- PTTWeb2/Web2Testing/src/test/java/App.java
+++ PTTWeb2/Web2Testing/src/test/java/App.java
@@ -3,9 +3,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.setProperty("webdriver.chrome.driver", "./chromedriver");
+        System.setProperty("webdriver.chrome.driver", "./Web2Testing/chromedriver-windows");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
-        driver.quit();    
+
+        driver.quit();
     }
 }
