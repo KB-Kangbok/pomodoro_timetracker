d96d66745672a3f1895ab9e40d5332f9eaee3cc5
Saira Poonnen <spoonnen3@gatech.edu>
2021-11-17 21:13:44 -0500

    Delete Project Test

--- PTTMobile2/app/src/androidTest/java/com/example/tejasvedantham/pttmobile2/DeleteProjectActivityTest.java
+++ PTTMobile2/app/src/androidTest/java/com/example/tejasvedantham/pttmobile2/DeleteProjectActivityTest.java
@@ -1,12 +1,23 @@
 package com.example.tejasvedantham.pttmobile2;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.test.InstrumentationRegistry;
+import android.util.Log;
 import android.view.WindowManager;
 import android.widget.ListView;
+import android.widget.Toast;
 
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import junit.framework.TestCase;
 
@@ -16,6 +27,7 @@ import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.rule.ActivityTestRule;
 
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -24,8 +36,13 @@ import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 
 import org.hamcrest.Description;
 import org.hamcrest.TypeSafeMatcher;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.Random;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.typeText;
@@ -40,36 +57,439 @@ import static org.hamcrest.Matchers.instanceOf;
 public class DeleteProjectActivityTest extends TestCase {
 
 //    @Rule
-//    public ActivityTestRule<CreateProjectActivity> act = new ActivityTestRule<CreateProjectActivity>(UserHomeActivity.class, false, false);
+//    public ActivityTestRule<DeleteProjectActivity> act = new ActivityTestRule<DeleteeProjectActivity>(DeleteProjectActivityActivity.class, false, false);
+
+    private String projectName = "MyProject";
+
+
+    public String id = "";
+    public String firstname = "";
+    public String lastname = "";
+    public String username = "";
+
+    public final static ActivityTestRule<UserHomeActivity> deleteProject = new ActivityTestRule<UserHomeActivity>(UserHomeActivity.class, false, false);
+
+
+    @Test
+    public void testDeleteNoTime() {
+        removeAllCurrent();
+        try
+        {
+            Thread.sleep(1800);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+        createDummyUser();
+        try
+        {
+            Thread.sleep(1800);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+        addProjectUser("MyProj");
+        try
+        {
+            Thread.sleep(1800);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+        addProjectUser("MyProj2");
+        Intents.init();
+
+        try
+        {
+            Thread.sleep(1800);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+
+        Intent intent = new Intent();
+        intent.putExtra("id", id);
+
+        deleteProject.launchActivity(intent);
+
+        onData(anything())
+                .inAdapterView(withId(R.id.project_list))
+                .atPosition(0)
+                .onChildView(withId(R.id.deleteProject))
+                .perform(click());
+        try
+        {
+            Thread.sleep(1800);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+
+        onData(anything())
+                .inAdapterView(withId(R.id.project_list))
+                .atPosition(0)
+                .onChildView(withId(R.id.nameProject))
+                .check(matches(withText("MyProj2")));
+        deleteDummyUser();
+        Intents.release();
+
+    }
+    String projectId = "";
+
+    @Test
+    public void testDeleteWithTimeNoConfirm() {
+        removeAllCurrent();
+        try
+        {
+            Thread.sleep(1800);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+        createDummyUser();
+        try
+        {
+            Thread.sleep(1800);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+        addProjectUser("MyProj");
+        try
+        {
+            Thread.sleep(1800);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+        addProjectUser("MyProj2");
+        Intents.init();
+
+        try
+        {
+            Thread.sleep(1800);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+        addTimeToProj();
+
+        Intent intent = new Intent();
+        intent.putExtra("id", id);
+
+        deleteProject.launchActivity(intent);
+
+        onData(anything())
+                .inAdapterView(withId(R.id.project_list))
+                .atPosition(0)
+                .onChildView(withId(R.id.deleteProject))
+                .perform(click());
+        try
+        {
+            Thread.sleep(1800);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+        onView(withText("The project has time already logged to it. Do you still want to delete it?")).check(matches(isDisplayed()));
+        onView(withText("NO")).perform(click());
+        onData(anything())
+                .inAdapterView(withId(R.id.project_list))
+                .atPosition(0)
+                .onChildView(withId(R.id.nameProject))
+                .check(matches(withText("MyProj")));
+        onData(anything())
+                .inAdapterView(withId(R.id.project_list))
+                .atPosition(1)
+                .onChildView(withId(R.id.nameProject))
+                .check(matches(withText("MyProj2")));
+
+        Intents.release();
+
+    }
+
+    @Test
+    public void testDeleteWithTimeYesConfirm() {
+        projectId = "";
+        removeAllCurrent();
+        try
+        {
+            Thread.sleep(1800);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+        createDummyUser();
+        try
+        {
+            Thread.sleep(1800);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+        addProjectUser("MyProj");
+        try
+        {
+            Thread.sleep(1800);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+        addProjectUser("MyProj2");
+        Intents.init();
+
+        try
+        {
+            Thread.sleep(1800);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+        addTimeToProj();
+
+        Intent intent = new Intent();
+        intent.putExtra("id", id);
+
+        deleteProject.launchActivity(intent);
+
+        onData(anything())
+                .inAdapterView(withId(R.id.project_list))
+                .atPosition(0)
+                .onChildView(withId(R.id.deleteProject))
+                .perform(click());
+        try
+        {
+            Thread.sleep(1800);
+        }
+        catch(InterruptedException e)
+        {
+
+        }
+        onView(withText("The project has time already logged to it. Do you still want to delete it?")).check(matches(isDisplayed()));
+        onView(withText("YES")).perform(click());
+        onData(anything())
+                .inAdapterView(withId(R.id.project_list))
+                .atPosition(0)
+                .onChildView(withId(R.id.nameProject))
+                .check(matches(withText("MyProj2")));
+
+
+        Intents.release();
+
+    }
+
+    private void createDummyUser() {
+
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("firstName", "Minyi");
+            postData.put("lastName", "Lu");
+
+            postData.put("email", generateEmail());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+//        Log.d(LOG_TAG, "create user: " + postData.toString());
+
+        String url = BackendConnections.baseUrl + "/users";
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                Log.d("yes", "POST /users RES " + response);
+//                myMethod(response.toString()).
+
+                try {
+                    username =  response.get("email").toString();
+                    Log.d("name", username);
+                    id =  response.get("id").toString();
+                    Log.d("ID", id);
+//                    System.out.println(id);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("errored");
+                Log.d("not", "POST /users REQ FAILED");
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+
+    }
+
+    private void deleteDummyUser() {
+
+        String url = BackendConnections.baseUrl + String.format("/users/%s", id);
+//        Log.d(LOG_TAG, "delete user url: " + url);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.DELETE, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+//                Log.d(LOG_TAG, String.format("DELETE %s RES %s", url, response.toString()));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+//                Log.d(LOG_TAG, String.format("DELETE %s FAILED", url));
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+
+    }
+    private String generateEmail() {
+        String email = "";
+        Random r = new Random();
+        for (int i = 0; i < 50; i++) {
+            email = email + r.nextInt(10);
+        }
+        return email;
+    }
+    private void removeAllCurrent() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        String url = BackendConnections.baseUrl + "/users";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray jsonArray) {
+                        Log.d("get", String.format("GET %s RES %s", url, jsonArray.toString()));
+                        try {
+                            JSONArray users = jsonArray;
+                            boolean userFound = false;
+                            for (int i = 0; i < users.length(); ++i) {
+                                JSONObject user = (JSONObject) users.get(i);
+                                String id = (user.get("id").toString());
+                                deleteUser(id);
+
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    private void deleteUser(String id) {
+
+        String url = BackendConnections.baseUrl + String.format("/users/%s", id);
+//        Log.d(LOG_TAG, "delete user url: " + url);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.DELETE, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("DELETE", String.format("DELETE %s RES %s", url, response.toString()));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+//                Log.d(LOG_TAG, String.format("DELETE %s FAILED", url));
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+
+    }
+
+    private void addProjectUser(String name){
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("projectname", name);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+//        Log.d(LOG_TAG, "create user: " + postData.toString());
+
+        String url = BackendConnections.baseUrl + "/users/" + id + "/projects";
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                Log.d("yes", "POST /projects RES " + response);
+//                myMethod(response.toString()).
+
+                try {
+                    if (projectId == "") {
+                        projectId = response.get("id").toString();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("errored");
+                Log.d("not", "POST /users REQ FAILED");
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    private void addTimeToProj(){
+        String url = BackendConnections.baseUrl + String.format("/users/%s/projects/%s/sessions", id, projectId);
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("startTime", "2019-02-18T20:00Z");
+            postData.put("endTime", "2019-02-18T21:00Z");
+            postData.put("counter", "0");
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+//        Log.d(LOG_TAG, "create user: " + postData.toString());
+
+//        String url = BackendConnections.baseUrl + "/users/" + id + "/projects";
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                Log.d("yes", "POST /projects RES " + response);
+//                myMethod(response.toString()).
 //
-//    private String projectName = "MyProject";
-//
-//    public void setUp() throws Exception {
-//        super.setUp();
-//        Intents.init();
-//    }
-//
-//    @Test
-//    public void testDeleteProject() {
-//        Intent intent = new Intent();
-//        intent.putExtra("id", "1");
-//
-//        act.launchActivity(intent);
-//
-//        intended(hasComponent(UserHomeActivity.class.getName()));
-//
-//        onData(anything())
-//                .inAdapterView(withId(R.id.project_list))
-//                .atPosition(0)
-//                .onChildView(withId(R.id.deleteProject))
-//                .check(matches(isDisplayed()))
-//                .perform(click());
-//
-//    }
-//
-//
-//
-//    public void tearDown() throws Exception {
-//        Intents.release();
-//    }
+//                try {
+////                    projectId = response.get("id").toString();
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("errored");
+                Log.d("not", "POST /users REQ FAILED");
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
+
 }
